159d4e3a74f192e2645274dc8779ac7ccead34f2
Gustavo Pastorelli <gpastorelli89@gmail.com>
2017-06-25 22:50:47 -0400

    Add task status model

--- invgate/twitterapi/models.py
+++ invgate/twitterapi/models.py
@@ -9,10 +9,26 @@ class TwitterProfile(models.Model):
         Defines the twitter profile information used in the crawler
     """
     def __str__(self):
-        return "{} <{}>".format(self.name, self.short_description)
+        return u"{} <{}>".format(self.name, self.short_description)
 
     username = models.CharField(max_length=100, default="")
     name = models.CharField(max_length=100, unique=True)
     short_description = models.CharField(max_length=200, blank=True, null=True)
     profile_pic_uri = models.CharField(max_length=200, blank=True, null=True)
     popularity_index = models.IntegerField(default=0)
+
+
+class TwitterTaskStatus(models.Model):
+    """
+        Holds the status of the request twitter task.
+        INFO:
+        username: Twitter profile requested
+        job: Job id
+        status: Twitter API response status (default 202 pending)
+        twitter_response: Twitter API response message
+    """
+    username = models.CharField(max_length=100, unique=True)
+    job = models.CharField(max_length=200, unique=True)
+    status = models.IntegerField(default=202)
+    twitter_response = models.TextField(blank=True, null=True)
+
